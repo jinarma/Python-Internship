1aa7b39013380de78e94da52085be59460228fc7
jinarma <jinarma@gmail.com>
2021-08-04 13:01:47 +0530

    basically test code

--- Day 3/test.py
+++ Day 3/test.py
@@ -1,3 +1,11 @@
 #reverse for loop
 for i in range(10, 0, -1):
 	print(i)
+list1 = [1, 2, 3, '2', 5, 6]
+print(list1[::-1])
+a = [0]*5
+string1 = 'king'
+print([a])
+print(a+list1)
+list1.append(string1)
+print(list1)
