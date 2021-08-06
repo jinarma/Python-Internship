9c6ec1f9a7ba7faccb1b55acbb6508b9f9f85717
jinarma <jinarma@gmail.com>
2021-08-06 15:20:44 +0530

    completed example

--- Day 4/Day 4 - 3.py
+++ Day 4/Day 4 - 3.py
@@ -1,5 +1,18 @@
 #tuple duplication, slicing and check if mutable
 tuple1 = ("spiders", 'are', 'scary')*3
+
 print(tuple1)
 print(tuple1[3:])
-print(tuple1[3:6])
+print(tuple1[3:5])
+
+tuple2 = ('spiders', 'not', 'scary')*3
+
+tuple3 = tuple1+tuple2
+
+print(tuple3)
+
+tuple4 = ('is', 'this', 'the', 'best', 'you', 'got')
+
+print(tuple4)
+
+# tuple4.append('boi')	#this line ends in error
