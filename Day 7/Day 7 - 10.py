f9865b625df62391a6bbc64d7e21c30f38b3707a
jinarma <jinarma@gmail.com>
2021-08-12 13:04:44 +0530

    added area of circle as example

--- Day 7/Day 7 - 10.py
+++ Day 7/Day 7 - 10.py
@@ -1,4 +1,9 @@
 from math import pi as PI
-import math
 
-print(PI)
+def area_of_circle(radius):
+	return PI*pow(radius, 2)
+
+R = int(input('Enter the radius of circle (cm): '))
+
+area = area_of_circle(R)
+print('{%.2f} sqcm'.format(area))
