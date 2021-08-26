17bde04ea249d971487f83db084b81eb71c452c1
jinarma <jinarma@gmail.com>
2021-08-26 14:01:20 +0530

    used pop Co-authored-by: SupaHotPie <SupaHotPie@users.noreply.github.com>

--- Testing/mapping.py
+++ Testing/mapping.py
@@ -1,2 +1,9 @@
-num = input("Enter the number : ")
-print(list(map(int, num)))
+num = "123456"
+my_list = list(map(int, num))
+
+sum1 = my_list.pop(0) + my_list.pop()
+sum2 = 0
+while my_list:
+	sum2 += my_list.pop()
+print(sum1, sum2)
+
