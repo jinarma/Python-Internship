ffcb2a313662a6ae622d32530682a609b2ebe473
jinarma <jinarma@gmail.com>
2021-08-26 09:30:01 +0530

    Added info

--- Day 3/Day 3 - 5.py
+++ Day 3/Day 3 - 5.py
@@ -1,6 +1,7 @@
 num = [1, 2, 3, 4]
-stg = ['python', 'language']
-num.extend(stg)
+stg = ['python', 'language', ['easy']]
+num.extend(stg)		#extend adds elements from the iterable for one level; takes multiple inputs as args
+num.append([8, 19])	#takes only one input whether one iterable of multiple elements or just one element and adds it to the end of the list
 #num += stg
 num.insert(4, 'good')
 print(num)
