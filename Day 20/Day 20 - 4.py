6bb6fcd105a2605f067a303a68d30e9600fbb6b4
jinarma <jinarma@gmail.com>
2021-08-28 10:11:58 +0530

    first commit

---
+++ Day 20/Day 20 - 4.py
@@ -0,0 +1,24 @@
+""" 
+A famous company is going to select a new Managing Director. All the eligible candidates are waiting in the meeting room. Everyone is seated in a random order linearly. Everyone are cunning and want's to become the Managing Director. If the founder of that company tell's a person name directly definitely that is going to become a serious problem. But the founder already have a person on his mind. But he was thinking for a creative idea to convey the same. Suddenly he notices a pattern follows and he find a way to tell who is the new MD.
+He say's "the next MD of this company is the person who haves the age greater than all the other person to his right side."
+Write the program to find the new MD of that company where you are provided with the n persons age in order similar to their seating arrangement. In case if the founder couldn't find the next MD print, "The promotion stands cancelled".
+
+Note: There will be at least 2 person waiting in the meeting room.
+ """
+
+
+def select_MD(persons_list):
+
+	for i in range(len(persons_list)):
+		if persons_list[i] == max(persons_list[i:]):
+			print(persons_list[i])
+			return
+	return
+
+
+select_MD([2, 3, 12, 4, 24, 120, 23])
+		
+
+	
+
+
